ba82f1c567d8be0d9644f9014953266d14b19565
satramadhani <satramadhani@outlook.com>
2026-06-04 11:12:55 +0700

    init: Create project

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version: 6.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Containerize",
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .executableTarget(
+            name: "Containerize",
+            dependencies: [
+                .product(name: "ArgumentParser", package: "swift-argument-parser"),
+            ]
+        ),
+        .testTarget(
+            name: "ContainerizeTests",
+            dependencies: ["Containerize"]
+        ),
+    ],
+    swiftLanguageModes: [.v6]
+)
