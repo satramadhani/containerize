e2a05bb7c22de32f64ad0336118f8a1fbfabeb90
satramadhani <satramadhani@outlook.com>
2026-06-08 08:46:46 +0700

    feat: Add array of string format support for environment option

--- Sources/Containerize/Models/Service.swift
+++ Sources/Containerize/Models/Service.swift
@@ -6,6 +6,44 @@ struct Service : Codable {
     let volumes: [String]?
     let dependsOn: [String]?
 
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        if container.contains(.image) {
+            image = try container.decode(String.self, forKey: .image)
+            build = nil
+        } else if container.contains(.build) {
+            build = try container.decode(Build.self, forKey: .build)
+            image = nil
+        } else {
+            build = nil
+            image = nil
+        }
+
+        if container.contains(.environment) {
+            var parsed: [String: String] = [:]
+            
+            if let dict = try? container.decode([String: String].self, forKey: .environment) {
+                parsed = dict
+            } else if let array = try? container.decode([String].self, forKey: .environment) {
+                for entry in array {
+                    let parts = entry.split(separator: "=", maxSplits: 1)
+                    if parts.count == 2 {
+                        parsed[String(parts[0])] = String(parts[1])
+                    }
+                }
+            }
+
+            environment = parsed
+        } else {
+            environment = nil
+        }
+        
+        ports = try container.decodeIfPresent([String].self, forKey: .ports)
+        volumes = try container.decodeIfPresent([String].self, forKey: .volumes)
+        dependsOn = try container.decodeIfPresent([String].self, forKey: .dependsOn)
+    }
+
     enum CodingKeys: String, CodingKey {
         case image
         case build
