998bf8f5974c0b9abbbf1808950415857d172bf6
satramadhani <satramadhani@outlook.com>
2026-06-09 08:45:54 +0700

    feat: Add minimal custom runtime path configuration.

--- Sources/Containerize/Containerize.swift
+++ Sources/Containerize/Containerize.swift
@@ -1,7 +1,14 @@
 import ArgumentParser
+import Foundation
 
 @main
 struct Containerize: ParsableCommand {
+    @Option(
+        name: .shortAndLong,
+        help: "Path to the container runtime."
+    )
+    var runtime: String?
+
     static let configuration: CommandConfiguration = CommandConfiguration(
         abstract: "A personal 'docker compose'-inspired tool for Apple's container.",
         version: "0.0.1",
@@ -13,4 +20,9 @@ struct Containerize: ParsableCommand {
             Stop.self
         ]
     )
+
+    mutating func run() throws {
+        let envPath = ProcessInfo.processInfo.environment["CONTAINER_PATH"]
+        Runner.runtimePath = runtime ?? envPath ?? Runner.runtimePath
+    }
 }
