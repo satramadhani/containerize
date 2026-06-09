5bdae668f9f15ff2ffc551c938895a0fa27fc180
satramadhani <satramadhani@outlook.com>
2026-06-09 08:39:42 +0700

    feat: Add runQuiet method in runner to suppress container's internal log

--- Sources/Containerize/Runner.swift
+++ Sources/Containerize/Runner.swift
@@ -1,10 +1,24 @@
 import Foundation
 
-struct Runner {
+struct Runner {    
+    // TODO: Look for other solutions for shared mutable state.
+    nonisolated(unsafe) static var runtimePath: String = "/usr/local/bin/container"
+
     static func run(_ arguments: [String]) throws {
         let process = Process()
-        process.executableURL = URL(fileURLWithPath: "/usr/local/bin/container")
+        process.executableURL = URL(fileURLWithPath: runtimePath)
         process.arguments = arguments
+
+        try process.run()
+        process.waitUntilExit()
+    }
+
+    static func runQuiet(_ arguments: [String]) throws {
+        let process = Process()
+        process.executableURL = URL(fileURLWithPath: runtimePath)
+        process.arguments = arguments
+        process.standardError = FileHandle.nullDevice
+        process.standardOutput = FileHandle.nullDevice
 
         try process.run()
         process.waitUntilExit()
